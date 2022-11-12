7a767ae0dfd7c1c1ed1829d25d89ef12938a2e55
Omar Ruiz <oruiz@lsq.com>
2022-11-12 14:04:22 -0600

    adding dashboard incomplete

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ from google.oauth2 import service_account
 from googleapiclient.discovery import build
 from googleapiclient.http import HttpRequest
 
+from dashboard import gastos_category, pago_pngi, deuda_mes
+
 
 SCOPE = "https://www.googleapis.com/auth/spreadsheets"
 SPREADSHEET_ID = "1smARqiOwAgsFZhNTJAuWPwkJuJ4mwp9tvgClElOFINk"
@@ -54,6 +56,9 @@ def get_data(gsheet_connector) -> pd.DataFrame:
     df = pd.DataFrame(values["values"])
     df.columns = df.iloc[0]
     df = df[1:]
+    df["Cargo"] = df["Cargo"].astype(float)
+    df["fecha"] = pd.to_datetime(df["fecha de operacion"], format="%Y-%m-%d")
+
     return df
 
 
@@ -88,44 +93,68 @@ def tipo_de_pago():
     return ["B", "BO", "CB", "CI", "DL", "DO", "E", "PL", "PO"]
 
 
+# -------------------------
+
 st.set_page_config(page_title="Finanzas", page_icon="🐞", layout="centered")
+registros, dashboard = st.tabs(["Registros", "Dashboard"])
 
-st.title("🐞 Registro de gastos")
+with registros:
 
-gsheet_connector = connect_to_gsheet()
+    st.title("🐞 Registro de gastos")
+    gsheet_connector = connect_to_gsheet()
 
-st.sidebar.write(
-    f"This app shows how a Streamlit app can interact easily with a [Google Sheet]({GSHEET_URL}) to read or store data."
-)
-
-st.sidebar.write(
-    f"[Read more](https://docs.streamlit.io/knowledge-base/tutorials/databases/public-gsheet) about connecting your Streamlit app to Google Sheets."
-)
-
-form = st.form(key="annotation")
-
-with form:
-    cols = st.columns((1, 1))
-    cargo = cols[0].text_input("Monto:")
-    etiqueta = cols[1].selectbox("Etiqueta:", etiquetas(), index=2)
-    establecimiento = st.text_input("Establecimiento:")
-    date = st.date_input("Fecha de operacion:")
-    cols = st.columns((1, 1))
-    tipo_pago = cols[0].selectbox("Tipo de pago:", tipo_de_pago(), index=2)
-    msi = cols[1].checkbox("MSI")
-
-    submitted = st.form_submit_button(label="Submit")
-
-
-if submitted:
-    add_row_to_gsheet(
-        gsheet_connector,
-        [[cargo, etiqueta, establecimiento, str(date), tipo_pago, msi]],
+    st.sidebar.write(
+        f"This app shows how a Streamlit app can interact easily with a [Google Sheet]({GSHEET_URL}) to read or store data."
     )
-    st.success("Gracias! Tucargo ha sido registrado.")
-    st.balloons()
+    st.sidebar.write(
+        f"[Read more](https://docs.streamlit.io/knowledge-base/tutorials/databases/public-gsheet) about connecting your Streamlit app to Google Sheets."
+    )
 
-expander = st.expander("Ve todos los registros")
-with expander:
+    form = st.form(key="annotation")
+
+    with form:
+        cols = st.columns((1, 1))
+        cargo = cols[0].text_input("Monto:")
+        etiqueta = cols[1].selectbox("Etiqueta:", etiquetas(), index=2)
+        establecimiento = st.text_input("Establecimiento:")
+        date = st.date_input("Fecha de operacion:")
+        cols = st.columns((1, 1))
+        tipo_pago = cols[0].selectbox("Tipo de pago:", tipo_de_pago(), index=2)
+        msi = cols[1].checkbox("MSI")
+
+        submitted = st.form_submit_button(label="Submit")
+
+    if submitted:
+        add_row_to_gsheet(
+            gsheet_connector,
+            [[establecimiento, cargo, str(date), etiqueta, tipo_pago, msi]],
+        )
+        st.success("Gracias! Tu cargo ha sido registrado.")
+        st.balloons()
+
+    # expander = st.expander("Ve todos los registros")
+    # with expander:
     st.write(f"Open original [Google Sheet]({GSHEET_URL})")
     st.dataframe(get_data(gsheet_connector))
+
+with dashboard:
+    st.title("🐞 Dashboard")
+
+    st.subheader("Total gastos del mes y periodo anterior")
+
+    tab = deuda_mes(df=get_data(gsheet_connector), income=117000)
+    gpa, gma_no_fijos = st.columns(2)
+    gpa.metric(
+        "Para no generar intereses (GPNGI)",
+        f'{tab.loc["Total GPA", "Monto"]} MXN',
+    )
+    gma_no_fijos.metric(
+        "Gastos No Fijos del mes anterior fuera de GPNGI ",
+        f'{tab.loc["Total GMA No Fijos del periodo", "Monto"]} MXN',
+    )
+    # col2.metric("Wind", "9 mph", "-8%")
+    # col3.metric("Humidity", "86%", "4%")
+
+    # expander = st.expander("Ve todos los registros")
+    # with expander:
+    st.dataframe(deuda_mes(df=get_data(gsheet_connector), income=117000))
---
+++ dashboard.py
@@ -0,0 +1,186 @@
+import pandas as pd
+
+
+cortes = {
+    "B": 24,
+    "BO": 4,
+    "CB": 16,
+    "CI": 5,
+    "PL": 1,
+    "PO": 1,
+    "E": 1,
+    "DL": 1,
+    "DO": 1,
+}
+
+
+def pago_pngi(df, cortes, month=None, year=None):
+
+    res = {i: [] for i in cortes.keys()}
+
+    if month is None:
+        current_month = pd.to_datetime("today").strftime("%m")
+    else:
+        current_month = month
+
+    if year is None:
+        current_year = pd.to_datetime("today").strftime("%y")
+    else:
+        current_year = year
+
+    first_day_current_month = pd.to_datetime(
+        f"{1}/{int(current_month)}/{current_year}", format="%d/%m/%y"
+    )
+    first_day_last_month = pd.to_datetime(
+        f"{1}/{int(current_month) - 1}/{current_year}", format="%d/%m/%y"
+    )
+
+    for tarjeta, corte in cortes.items():
+        if tarjeta in ["B", "CB"]:
+            corte_min = pd.to_datetime(
+                f"{corte}/{int(current_month) - 2}/{current_year}",
+                format="%d/%m/%y",
+            )
+            corte_max = pd.to_datetime(
+                f"{corte}/{int(current_month) - 1}/{current_year}",
+                format="%d/%m/%y",
+            )
+        else:
+            corte_min = pd.to_datetime(
+                f"{corte}/{int(current_month) - 1}/{current_year}",
+                format="%d/%m/%y",
+            )
+            corte_max = pd.to_datetime(
+                f"{corte}/{int(current_month)}/{current_year}",
+                format="%d/%m/%y",
+            )
+
+        keep = df["tipo de pago"] == tarjeta
+        if tarjeta in ["E", "DL", "DO"]:
+            keep &= df["fecha"] >= corte_min
+            keep &= df["fecha"] < corte_max
+        else:
+            keep &= df["fecha"] > corte_min
+            keep &= df["fecha"] <= corte_max
+        #
+        # print(tarjeta, round(df['Cargo'][keep].sum(), 2))
+        res[tarjeta].append(round(df["Cargo"][keep].sum(), 2))
+
+        keep_gf = (df["etiqueta"] == "Gasto fijo") & (keep)
+        res[tarjeta].append(round(df["Cargo"][keep_gf].sum(), 2))
+
+        keep_gnf = (df["etiqueta"] != "Gasto fijo") & (keep)
+        res[tarjeta].append(round(df["Cargo"][keep_gnf].sum(), 2))
+
+        keep = (
+            (df["tipo de pago"] == tarjeta)
+            & (df["fecha"] >= first_day_last_month)
+            & (df["fecha"] < first_day_current_month)
+        )
+        res[tarjeta].append(round(df["Cargo"][keep].sum(), 2))
+
+        keep = (
+            (df["tipo de pago"] == tarjeta)
+            & (df["fecha"] >= first_day_last_month)
+            & (df["fecha"] < first_day_current_month)
+            & (df["etiqueta"] == "Gasto fijo")
+        )
+        res[tarjeta].append(round(df["Cargo"][keep].sum(), 2))
+
+        keep = (
+            (df["tipo de pago"] == tarjeta)
+            & (df["fecha"] >= first_day_last_month)
+            & (df["fecha"] < first_day_current_month)
+            & (df["etiqueta"] != "Gasto fijo")
+        )
+        res[tarjeta].append(round(df["Cargo"][keep].sum(), 2))
+
+        keep_gnf_periodo = (
+            (df["tipo de pago"] == tarjeta)
+            & (df["fecha"] > corte_max)
+            & (df["fecha"] < first_day_current_month)
+            & (df["etiqueta"] != "Gasto fijo")
+        )
+        res[tarjeta].append(round(df["Cargo"][keep_gnf_periodo].sum(), 2))
+
+    columns = [
+        "Gastos del periodo anterior (GPA)",
+        "GPA Fijos",
+        "GPA No Fijos",
+        "Gastos del mes anterior (GMA)",
+        "GMA Fijos",
+        "GMA No Fijos",
+        "GMA No Fijos del periodo",
+    ]
+    tab = pd.DataFrame.from_dict(res, orient="index", columns=columns)
+    tab.loc["Total"] = tab.sum()
+
+    return tab
+
+
+def gastos_category(df, cortes, month=None, year=None):
+
+    res = {i: [] for i in cortes.keys()}
+
+    if month is None:
+        current_month = pd.to_datetime("today").strftime("%m")
+    else:
+        current_month = month
+
+    if year is None:
+        current_year = pd.to_datetime("today").strftime("%y")
+    else:
+        current_year = year
+
+    first_day_current_month = pd.to_datetime(
+        f"{1}/{int(current_month)}/{current_year}", format="%d/%m/%y"
+    )
+    first_day_last_month = pd.to_datetime(
+        f"{1}/{int(current_month) - 1}/{current_year}", format="%d/%m/%y"
+    )
+
+    for tarjeta in res.keys():
+        for etiqueta in set(df["etiqueta"]):
+            keep = (
+                (df["tipo de pago"] == tarjeta)
+                & (df["fecha"] >= first_day_last_month)
+                & (df["fecha"] < first_day_current_month)
+                & (df["etiqueta"] == etiqueta)
+            )
+            res[tarjeta].append(round(df["Cargo"][keep].sum(), 2))
+
+    columns = list(set(df["etiqueta"]))
+    tab = pd.DataFrame.from_dict(res, orient="index", columns=columns)
+    tab.loc["Total"] = tab.sum()
+    tab["Total"] = tab.sum(axis=1)
+
+    return tab
+
+
+def deuda_mes(df, income=None):
+
+    lo_que_se_debe_pagar_este_mes = {}
+    tab = pago_pngi(df, cortes)
+    lo_que_se_debe_pagar_este_mes["Total GPA"] = tab[
+        "Gastos del periodo anterior (GPA)"
+    ][["B", "BO", "CB", "CI", "PL", "PO"]].sum()
+    lo_que_se_debe_pagar_este_mes["Total GMA No Fijos del periodo"] = tab[
+        "GMA No Fijos del periodo"
+    ][["B", "BO", "CB", "CI", "PL", "PO"]].sum()
+    tab_p = pago_pngi(df, cortes, month="12", year="22")
+    lo_que_se_debe_pagar_este_mes[
+        "Total GMA Fijos del periodo en Debito/efectivo"
+    ] = tab_p["GMA Fijos"][["E", "DL", "DO"]].sum()
+    lo_que_se_debe_pagar_este_mes["Total necesario para este mes"] = round(
+        sum(list(lo_que_se_debe_pagar_este_mes.values())), 2
+    )
+    lo_que_se_debe_pagar_este_mes["Disponible para este mes"] = round(
+        income - lo_que_se_debe_pagar_este_mes["Total necesario para este mes"],
+        2,
+    )
+
+    tab = pd.DataFrame.from_dict(
+        lo_que_se_debe_pagar_este_mes, orient="index", columns=["Monto"]
+    )
+
+    return tab
